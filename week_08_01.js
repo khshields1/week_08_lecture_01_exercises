65aa06f596f3620c18a8413a94bcc2d80855fbcf
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-03-29 20:51:42 -0500

    week_08_lecture_01_exercises exercise answers and short answers

---
+++ week_08_01.js
@@ -0,0 +1,40 @@
+// Exercise 1
+
+var spanSelector = document.querySelectorAll('span')
+console.log(spanSelector)
+
+// Exercise 2
+
+var divClassFooSelector = document.getElementsByClassName('foo')
+console.log(divClassFooSelector)
+
+// Exercise 3
+
+var newList = document.getElementById('ol')
+newList.removeChild(newList.childNodes[4])
+newList.HTMLElementObject.title
+
+// Exercise 4
+
+
+
+// Delayed Reaction Exercise
+
+function startle () {
+  return ('AAAACK!')
+}
+setTimeout(startle, 2000)
+
+// Time Clock Exercise
+
+
+
+/* Short Answer Exercise
+
+1) The DOM allows you to access and modify HTML files using JavaScript through a web browser.
+
+2) A node is an object that matches an HTML element on the DOM tree
+
+3) The window object represents the browser window
+
+4) The window object contains global properties */
